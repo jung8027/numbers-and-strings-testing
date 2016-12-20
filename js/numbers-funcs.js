a0aa74686c6a77e9e569f2d652ea956d0b804549
Jung Chen <jungwchen@gmail.com>
2016-12-20 18:00:32 -0500

    testing submit

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,7 @@
 const add = (a, b) => a + b;
+const filterEvens = (array) => array.filter(element=>element%2===0)
 
 module.exports = {
-  add: add
+  add: add,
+  filterEvens: filterEvens
 };
--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const add = require('../js/numbers-funcs').add;
+const filterEvens = require('../js/numbers-funcs').filterEvens;
 
 describe('numbers tests', () => {
   it('add() should add two numbers', () => {
@@ -12,3 +13,14 @@ describe('numbers tests', () => {
   });
   //Add more tests here:
 });
+
+describe('filter tests', ()=>{
+	it('filterEvens() should take in an array of random numbers and only return array with even nums', () => {
+		var result1 = filterEvens([1,2,3,4,5,6]);
+		var result2 = filterEvens([-1,-2,-3,-4,-5,-6]);
+		var result3 = filterEvens([-3,-1,1,3]);
+		expect(result1).eql([2,4,6]);
+		expect(result2).eql([-2,-4,-6]);
+		expect(result3).eql([]);
+	})
+})
